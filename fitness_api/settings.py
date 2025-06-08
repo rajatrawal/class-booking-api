08518c5e911b3c82b7e79e69fc93cb0f62063449
Rajat <rwlrajat@gmail.com>
2025-06-08 23:18:15 +0530

    allowed all host

--- fitness_api/settings.py
+++ fitness_api/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-t4)gj#4z9r#+!bf-1_(svea=70msdpwbif^$+oqybkmh6+@m-y
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
